27db217558ba600778b4dd39e0e35e70c349c026
Fedor Nikolaev <fsqcds@gmail.com>
2011-05-29 19:38:29 +0400

    make simple test for number operations properties

--- numbers.lisp
+++ numbers.lisp
@@ -1,2 +1,9 @@
 (defun gen-number (start end)
   (+ start (random (- (1+ end) start))))
+
+(deftest failtest ()
+  (check-for-all ((a (gen-number 5 10))
+		  (b (gen-number -5 5)))
+    (= (+ a b) (+ b a))
+    (= (- a b) (- b a))
+    ))
