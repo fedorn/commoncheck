c2b9db9c6e59d78790c2cce0f3d5a86430b63abc
Fedor Nikolaev <fsqcds@gmail.com>
2011-05-29 23:35:55 +0400

    deleted:    numbers.lisp 	deleted:    numbers.lisp~

--- numbers.lisp
+++
@@ -1,9 +0,0 @@
-(defun gen-number (start end)
-  (+ start (random (- (1+ end) start))))
-
-(deftest failtest ()
-  (check-for-all ((a (gen-number 5 10))
-		  (b (gen-number -5 5)))
-    (= (+ a b) (+ b a))
-    (= (- a b) (- b a))
-    ))
